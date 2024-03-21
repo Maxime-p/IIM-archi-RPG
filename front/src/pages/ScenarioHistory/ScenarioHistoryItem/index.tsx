5435607f7fb4976e7ec897636cd5bc379619898e
Timothée DURAND <timothee.durand70@gmail.com>
2024-03-21 13:51:53 +0100

    fix: build

--- front/src/pages/ScenarioHistory/ScenarioHistoryItem/index.tsx
+++ front/src/pages/ScenarioHistory/ScenarioHistoryItem/index.tsx
@@ -7,8 +7,8 @@ export const ScenarioHistoryItem: FC<{ scenario: ScenarioHistory }> = ({
 }) => {
   return (
     <li key={scenario.id} className="scenario-history-item">
-      <h2>{scenario.brief.title}</h2>
-      <p className="scenario-history-item__context">{scenario.brief.context}</p>
+      <h2>{scenario.brief.name}</h2>
+      <p className="scenario-history-item__context">{scenario.brief.description}</p>
     </li>
   )
 }
